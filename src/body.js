3c92ad00f974191d017d1cf76c219789d6dfad55
Iryna Maslova <maslovai@hotmail.com>
2019-09-10 15:59:26 -0700

    state working properly

--- src/body.js
+++ src/body.js
@@ -4,33 +4,40 @@ class Body extends React.Component{
     constructor(props){
         super(props);
         this.state = {
+           currentCategory:'All', 
            options:[
                'All', 'View', 'Format', 'Help'
            ],
            commands:[
-               ['View', ['PrviousError','Previous Error'],['NextTask','Next Task'],['PreviousTask', 'Previous Task']],
-               ['Format',['AlignBottoms','Align Bottoms'],['AlignLefts','Align Lefts'],['AlignRights','Align Rights']]
-        
+               {Parent:'View', command:[['PreviousError','Previous Error'],['NextTask','Next Task'],['PreviousTask', 'Previous Task']]},
+               {Parent:'Format',command:['AlignBottoms - Align Bottoms','AlignLefts - Align Lefts','AlignRights -Align Rights']}
            ]
         }
-
+        this.findCategory = this.findCategory.bind(this);
         this.findVoiceCommand = this.findVoiceCommand.bind(this);
     }
-    findVoiceCommand(command){
-
+    findCategory(e){
+        this.setState({
+            currentCategory: e.target.value
+        })
+    }
+    findVoiceCommand(inputCommand){
+            console.log(inputCommand)
+           
     }
     render(){
         return(
             <div>
                 <form>
-                    <select name="Element">
+                    <select name="category" onChange={this.findCategory} value ={this.state.currentCategory}>
                         {
                             this.state.options.map((opt,i) => 
-                            { return(<option id = {i+'choice'} key={i}>{opt}</option>)}
+                            { return(<option id = {i+'choice'} value = {opt} key={i} >{opt}</option>)}
                             )
                         }
                     </select>
-                    <input type = 'text' id='inputBox' placeholder='enter command here'></input>
+                    <div>Selected category is : {this.state.currentCategory}</div>
+                    <input type = 'text' id='inputBox' placeholder='enter command here' onChange={()=>this.findVoiceCommand(inputBox.value)}></input>
                 </form>
                 <button onClick={this.findVoiceCommand}>Go!</button>
             </div>
